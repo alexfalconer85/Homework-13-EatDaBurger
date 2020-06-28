e9ede195f82ff544ff25fc751bc72e88b077e209
Alex Falconer <aef61685@gmail.com>
2020-06-28 14:45:18 -0600

    first

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Bacon Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Cheese Burger');
+INSERT INTO burgers
+    (name, eaten)
+VALUES
+    ('Blue Moon Burger', true);
+INSERT INTO burgers
+    (name, eaten)
+VALUES
+    ('Plain Burger', true);
+INSERT INTO burgers
+    (name, eaten)
+VALUES
+    ('PB&J Burger', true);
+
